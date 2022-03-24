4f8720e75ded10be3e6b3a55dedf62d6e432ebf9
Pol Sanz <me@polsanz.xyz>
2022-03-24 10:47:23 +0100

    Added error to formchk

--- gauss_formchk.py
+++ gauss_formchk.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     file_list = [chkfil for chkfil in os.listdir() if chkfil.endswith(".chk")]
 
+    if len(file_list) == 0:
+        gu.print_color("\nNo '.chk' files found. Aborting.\n", "red")
+
     print("\nConverting the following '.chk' files into '.fchk':")
     print(file_list)
 
